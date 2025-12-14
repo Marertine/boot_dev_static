92f97b19076a2ea7c4e9a486bc758f2474a1a6b7
Marertine <rhodrirhamantus@gmail.com>
2025-12-15 10:21:34 +1100

    Ch 2-4: Leaf Nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,3 +25,17 @@ class HTMLNode:
         #str_output += f"        (   Props: {self.props})"
         #return str_output
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value")
+        
+        if self.tag is None:
+            #return f"<{self.tag}>{self.value}</{self.tag}>"
+            return f"{self.value}"
+        else:               
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
