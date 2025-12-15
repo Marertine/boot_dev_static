367110fd35ab93ffd1862c67a6b9c50acaef14ea
Marertine <rhodrirhamantus@gmail.com>
2025-12-15 15:52:13 +1100

    Ch4-2: Block Types

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -89,13 +89,3 @@ def text_to_textnodes(text):
     nodes = split_nodes_link(nodes)
     return nodes
 
-def markdown_to_blocks(markdown):
-    list_of_blocks = []
-
-    for block in markdown.split("\n\n"):
-        if block == []:
-            continue
-        
-        list_of_blocks.append(block.strip())
-    
-    return list_of_blocks
