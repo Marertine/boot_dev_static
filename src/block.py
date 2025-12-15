c627c778469d43a80fe468b6d47146fc5fceb087
Marertine <rhodrirhamantus@gmail.com>
2025-12-16 08:39:07 +1100

    Ch4-3: Block to HTML (cheated)

--- src/block.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class BlockType(Enum):
-    PARAGRAPH = "paragraph"
-    HEADING = "heading"
-    CODE = "code"
-    QUOTE = "quote"
-    ULIST = "unordered_list"
-    OLIST = "ordered_list"  
