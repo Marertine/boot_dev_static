ab7c9013983efc1253c09792068e1cd4d632ec92
Marertine <rhodrirhamantus@gmail.com>
2025-12-17 08:44:35 +1100

    Ch5-1: Copy Static - Draft 1

--- src/main.py
+++ src/main.py
@@ -1,13 +1,33 @@
 from textnode import TextNode
 from textnode import TextType
+from os import path, listdir, mkdir
+from shutil import copy, rmtree
 
 def main():
-    test_text = "This is some anchor text"
-    test_type = TextType.LINK
-    test_url = "https://www.boot.dev"
+    str_destpath = "~/public/"
+    str_src_path = "~/static/"
 
-    test_node = TextNode(test_text, test_type, test_url)
-    print(test_node)
+
+    if path.exists(str_destpath):
+        rmtree(str_destpath)
+
+    if not path.exists(str_destpath):
+        mkdir(str_destpath)
+
+    fn_copy_files(str_src_path, str_destpath)
+
+
+def fn_copy_files(str_src_path, str_destpath):
+    for item in list(str_src_path):
+        if path.isdir(item):
+            mkdir(path.join(str_destpath, item))
+            fn_copy_files(path.join(str_src_path, item), str_destpath)
+
     
+        if path.isfile(item):
+            copy(path.join(str_src_path, item), path.join(str_destpath, item))
+
+
+
 
 main()
